e2327e28c16c22f26f7c2b5d39ea3f6234af4116
deshraj <deshrajdry@gmail.com>
2015-02-04 11:56:10 +0530

    models and database changed due to bugs

--- ncs/ncs/models.py
+++ ncs/ncs/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class userDetails(models.Model):
-	name = models.CharField(required = False)
+	name = models.CharField(max_length = 1000)
 	createdAt = models.DateField(default=datetime.date.today)
 	assignedAt = models.CharField(default=datetime.date.today)
-	count = models.IntField(required = True)
+	count = models.CharField(max_length = 1000)
